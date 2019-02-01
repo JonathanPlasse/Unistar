91fcff25bf46d8f65169a540121d3857272d5809
Jonathan Plasse <jonathan.plasse@live.fr>
2019-02-01 12:42:09 +0100

    explicit Xf,…,Nf,m

--- trajectoire/modelisation.py
+++ trajectoire/modelisation.py
@@ -11,13 +11,12 @@
 #
 
 # import matplotlib.pyplot as plt
-from numpy import array, sin, cos, pi, dot, atan  # , linspace, sign
+from numpy import array, sin, cos, pi, dot, atan
 import sys
 # from scipy.integrate import odeint
 
 u0, v0, w0, phi0, theta0, psi0, p0, q0, r0 = [0]*9
-Xf0, Yf0, Zf0, Lf0, Mf0, Nf0, Vvent0, epsilon0 = [0]*8
-m = 1.5
+
 g = 9.81
 rho0 = 1015
 Cx = 0
@@ -28,10 +27,14 @@ D = 0 # Diamètre de la fusée
 Dogive = D # Diamètre de l'ogive
 L = 0 # Envergure des ailerons
 EPaileron = 0 # Épaisseur des ailerons
-MS = 0 # Marge de stabilité
 lx = 0
 ly = MS*D
 lz = MS*D
+
+Xf = lambda t:146.7 if t<0.97 else 0
+Yf, Zf, Lf, Mf, Nf, Vvent, epsilon = [0]*7
+m = lambda t:1.5-t*0.0869 if t<0.97 else 1.5-0.0843
+MS = 0 # Marge de stabilité
 
 A = m*D*D**2/2
 B = m*LongueurTube**2/12
@@ -44,9 +47,8 @@ Vect0 = array([u0, v0, w0, phi0, theta0, psi0, p0, q0, r0])
 U0 = array([Xf0, Yf0, Zf0, Lf0, Mf0, Nf0, Vvent0, epsilon0])
 
 
-def F(Vect, U):
+def F(Vect, t):
     u, v, w, phi, theta, psi, p, q, r = Vect
-    Xf, Yf, Zf, Lf, Mf, Nf, Vvent, epsilon = U
 
     Vvent_x = Vvent*cos(epsilon)*cos(psi)*cos(theta)-Vvent*sin(epsilon)*sin(psi)
     Vvent_y = Vvent*cos(epsilon)*(sin(psi)*cos(phi)+sin(phi)*sin(theta))+Vvent*sin(epsilon)*cos(psi)*cos(phi)
@@ -65,9 +67,9 @@ def F(Vect, U):
     Ma = rho*Sreference*Vrz**2*Cza*alpha*lz/2
     Na = rho*Sreference*Vry**2*Cyb*beta*ly/2
 
-    du = 1/m*(Xa+Xf-m*g*sin(theta)+r*v-q*w)
-    dv = 1/m*(Ya+Yf+m*g*cos(theta)*sin(phi)+p*w-r*u)
-    dw = 1/m*(Za+Zf+m*g*cos(theta)*cos(phi)+q*u-p*v)
+    du = 1/m*(Xa+Xf(t)-m(t)*g*sin(theta)+r*v-q*w)
+    dv = 1/m*(Ya+Yf+m(t)*g*cos(theta)*sin(phi)+p*w-r*u)
+    dw = 1/m*(Za+Zf+m(t)*g*cos(theta)*cos(phi)+q*u-p*v)
     dp = 1/A*(La+Lf+(B-C)*q*r)
     dq = 1/B*(Ma+Mf+(C-A)*p*r)
     dr = 1/C*(Na+Nf+(A-B)*p*q)
